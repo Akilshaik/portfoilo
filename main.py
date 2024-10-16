3454b53fa0951740ac6b0b44efb3452992e4bbe5
Akil Shaikh <96439306+Akilshaik@users.noreply.github.com>
2024-10-16 22:44:51 +0530

    venve

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,url_for,send_file
 
-# Initialize the Flask app
+
 app = Flask(__name__)
+
 
 # Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
 
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
+
+#@app.route('/about.html')
+#def about():
+#    return render_template('about.html')
+
+#@app.route('/works.html')
+#def works():
+#    return render_template('works.html')
+#
+#@app.route('/contact.html')
+#def contact():
+#   return render_template('contact.html')
 
 
 # Run the app
